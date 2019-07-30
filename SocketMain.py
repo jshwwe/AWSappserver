942e738f7f87982b624dbdeb3eb58f1421be44da
jshwwe <53284699+jshwwe@users.noreply.github.com>
2019-07-30 14:40:47 +0800

    Add files via upload

---
+++ SocketMain.py
@@ -0,0 +1,20 @@
+import sys
+from SocketListen import *
+
+print("==================================")
+print("==  Welcome to fake app server  ==")
+print("==================================")
+
+port = input("input port number: ")
+users = input("input max number of simultaneous connections: ")
+dbhost = input("input ip address of database server: ")
+dbuser = input("input username of database server: ")
+dbpass = input("input password of database server: ")
+dbname = input("input dbname of database: ")
+
+opensocket = socketlistener("", int(port), int(users), dbhost, dbuser, dbpass, dbname)
+
+print("\n===== App server is up and running! =====")
+
+opensocket.openport()
+
